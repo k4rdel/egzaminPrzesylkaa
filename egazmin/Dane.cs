159ee5136091e842a45641dcda208a2e27b23d72
k4rde <k4rde@DESKTOP-FC8VTMA>
2024-02-07 00:15:58 +0100

    Dodaj pliki projektów.

---
+++ egazmin/Dane.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace egazmin
+{
+    class Dane : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public bool pocztowka;
+        public bool list;
+        public bool paczka;
+        public bool Pocztowka
+        {
+            get { return pocztowka; }
+            set
+            {
+                pocztowka = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(nameof(Pocztowka)));
+                }
+            }
+        }
+        public bool List
+        {
+            get { return list; }
+            set
+            {
+                list = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(nameof(List)));
+                }
+            }
+        }
+        public bool Paczka
+        {
+            get { return paczka; }
+            set
+            {
+                paczka = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(nameof(Paczka)));
+                }
+            }
+        }
+
+        public string ulica;
+        public string kodPocztowy;
+        public string miasto;
+
+        public string Ulica
+        {
+            get { return ulica; }
+            set
+            {
+                ulica = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(nameof(Ulica)));
+                }
+            }
+        }
+        public string KodPocztowy
+        {
+            get { return kodPocztowy; }
+            set
+            {
+                kodPocztowy = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(nameof(KodPocztowy)));
+                }
+            }
+        }
+        public string Miasto
+        {
+            get { return miasto; }
+            set
+            {
+                miasto = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(nameof(Miasto)));
+                }
+            }
+        }
+        public Dane()
+        {
+            this.Ulica = ulica;
+            this.KodPocztowy = kodPocztowy;
+            this.Miasto = miasto;
+            this.List = pocztowka;
+            this.Pocztowka = list;
+            this.Paczka = paczka;
+        }
+    }
+}
---
+++ egazmin/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace egazmin
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Dane dane = new Dane();
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = dane;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (dane.Paczka == true)
+            {
+                fota.Source = new BitmapImage(new Uri(@"E:\visual studio\egazmin\egazmin\paczka.jpg"));
+                cena.Content = "Cena: 10 zł";
+            }
+            else if (dane.Pocztowka == true)
+            {
+                fota.Source = new BitmapImage(new Uri(@"E:\visual studio\egazmin\egazmin\pocztówka.png"));
+                cena.Content = "Cena: 1 zł";
+            }
+            else if (dane.List == true)
+            {
+                fota.Source = new BitmapImage(new Uri(@"E:\visual studio\egazmin\egazmin\list.jpg"));
+                cena.Content = "Cena: 1,5 zł";
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            int numericValue;
+            bool isNumber = int.TryParse(dane.KodPocztowy, out numericValue);
+
+            if (dane.KodPocztowy.Length > 5 || dane.KodPocztowy.Length < 5 || dane.KodPocztowy == null)
+            {
+                MessageBox.Show("Nieprawidłowa liczba cyfr w kodzie pocztowym");
+            }
+            else if(isNumber == false)
+            {
+                MessageBox.Show("Kod pocztowy powinien się składać z samych cyfr");
+            }
+            else
+            {
+                MessageBox.Show("Dane przesyłki zostały wprowadzone");
+            }
+        }
+    }
+}
